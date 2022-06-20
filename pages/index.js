3798d7a149e721b97a847aa918bd121c107b5813
moonayyur <jm_kias@esi.dz>
2022-06-20 02:11:22 +0100

    fix sections and pages and add .ico

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import MainNavbar from "../components/MainNavbar";
-import APropos from "./APropos";
-import Contact from "./Contact";
-import Hero from "./Hero";
-import Sections from "./Sections";
+import APropos from "../sections/APropos";
+import Contact from "../sections/Contact";
+import Hero from "../sections/Hero";
+import Sections from "../sections/Sections";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <div className="">
+      <Head>
+        <title>Ta7wissa</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/ta7wissa2.ico" />
+      </Head>
       <MainNavbar />
       <Hero />
       <Sections />
